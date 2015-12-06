59436395e736fe07f74d99d4e6b3cf53ad82a052
Danny Burrows <burrows.danny@gmail.com>
2015-12-06 08:28:09 -0600

    Initial commit

---
+++ InmateSelection/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InmateSelection
+{
+    public sealed class Child
+    {
+        public string Name { get; set; }
+        public string DOC { get; set; }
+        public string Facility { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+    }
+}
